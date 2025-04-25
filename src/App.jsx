8905ec0fefe217b0191a624aa9b4a843f81f9cbe
Ziyodov Anvar <ziyodovanvar1999@gmail.com>
2025-04-25 16:54:53 +0900

    Feat: develop PostPage and UserPage components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { Button } from "@chakra-ui/react";
 import { Container } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import UserPage from "./pages/UserPage";
+import PostPage from "./pages/PostPage";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <>
-      <Container>
-        <Button>Hello </Button>
-      </Container>
-    </>
+    <Container maxW="620px">
+      <Header />
+      <Routes>
+        <Route path="/:username" element={<UserPage />} />
+        <Route path="/:username/post/:pid" element={<PostPage />} />
+      </Routes>
+    </Container>
   );
 }
 
