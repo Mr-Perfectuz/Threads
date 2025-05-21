d4bfd1832dd0aa6284dac047438e00e78a9577e6
Ziyodov Anvar <ziyodovanvar1999@gmail.com>
2025-05-21 16:41:27 +0900

    Chore: modify getUserProfile controller, update the userRoutes and useLogout components

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,14 +5,24 @@ import generateTokenAndSetCookie from "../utils/helpers/generateTokenAndSetCooki
 import { v2 as cloudinary } from "cloudinary";
 
 const getUserProfile = async (req, res) => {
-  const { username } = req.params;
+  const { query } = req.params;
   try {
-    const user = await User.findOne({
-      username: new RegExp(`^${username}$`, "i"),
-    })
-      .select("-password")
-      .select("-updatedAt");
+    let user;
+
+    // query is userId
+    if (mongoose.Types.ObjectId.isValid(query)) {
+      user = await User.findOne({ _id: query })
+        .select("-password")
+        .select("-updatedAt");
+    } else {
+      // query is username
+      user = await User.findOne({ username: query })
+        .select("-password")
+        .select("-updatedAt");
+    }
+
     if (!user) return res.status(404).json({ error: "User not found" });
+
     res.status(200).json(user);
   } catch (err) {
     res.status(500).json({ error: err.message });
